8f8aa101e2461effa20b2106a03f385b34599328
Mr. Fleming <efleming@stpatrick.org>
2019-03-07 12:59:50 -0600

    Add files via upload

---
+++ toggleFunctions.js
@@ -0,0 +1,54 @@
+function green25(){
+
+  const doc = document;
+  let divs = doc.querySelectorAll();
+
+  for(){
+    divs[].classList.add();
+    divs[].classList.remove();
+    divs[].classList.remove();
+    divs[].classList.remove();
+  }
+
+}
+
+
+function green50(){
+
+
+}
+
+
+function green75(){
+
+
+}
+
+
+function green100(){
+
+
+}
+
+
+function smallToggle(){
+const doc = document;
+let divs = doc.querySelectorAll();
+
+for(){
+  divs[].classList.add();
+  divs[].classList.remove();
+  divs[].classList.remove();
+}
+
+}
+
+
+function mediumToggle(){
+
+}
+
+
+function largeToggle(){
+
+}
